55ddce505a9f154d15ca0b5b4039e434e99190ed
Nolan Reda <nolan.reda@gmail.com>
2023-01-17 05:48:45 +0000

    started portfolio site

--- client/app.jsx
+++ client/app.jsx
@@ -1,8 +1,36 @@
 import React from 'react';
-import Home from './pages/home';
+import Navbar from './pages/navbar';
+import parseRoute from './lib/parse-route';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      route: parseRoute(window.location.hash)
+    };
+  }
+
+  componentDidMount() {
+    window.addEventListener('hashchange', () => {
+      this.setState({
+        route: parseRoute(window.location.hash)
+      });
+    });
+  }
+
   render() {
-    return <Home />;
+    return (
+      <>
+        <header>
+          <Navbar />
+        </header>
+        <main>
+          <div>
+            <h1>Hello, World! I&apos;m Nolan</h1>
+          </div>
+          <img src="" alt="" />
+        </main>
+      </>
+    );
   }
 }
