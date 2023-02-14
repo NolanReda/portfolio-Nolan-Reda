38a817a96c3dd8ac9d6d7690274a0ce94265cba2
Nolan Reda <nolan.reda@gmail.com>
2023-02-14 19:13:13 +0000

    completed site footer and cleaned up useless code from repo

--- client/app.jsx
+++ client/app.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Navbar from './pages/navbar';
 import parseRoute from './lib/parse-route';
-import AppContext from './lib/app-context';
 import PageContainer from './components/page-container';
 import Home from './pages/home';
 import Test from './pages/test';
@@ -15,14 +14,6 @@ export default class App extends React.Component {
     this.renderPage = this.renderPage.bind(this);
   }
 
-  componentDidMount() {
-    window.addEventListener('hashchange', () => {
-      this.setState({
-        route: parseRoute(window.location.hash)
-      });
-    });
-  }
-
   renderPage() {
     const { path } = this.state.route;
     if (path === '') {
@@ -34,17 +25,16 @@ export default class App extends React.Component {
   }
 
   render() {
-    const { route } = this.state;
-    const contextValue = { route };
     return (
-      <AppContext.Provider value={contextValue}>
+      <>
         <header className='sticky-top'>
           <Navbar />
         </header>
         <PageContainer>
           { this.renderPage() }
         </PageContainer>
-      </AppContext.Provider>
+      </>
+
     );
   }
 }
